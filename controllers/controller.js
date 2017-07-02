49a1d27a817178cc74cd2fd7ef20e97c690430de
Laura McGinn <mcginn.laura@gmail.com>
2017-07-02 16:10:47 -0500

    basic templates and styles running

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,6 +1,11 @@
 module.exports = function(app) {
+
+	let things = {
+		name: art;
+	}
+
 	app.get('/', function(req,res) {
-		res.render('index');
+		res.render('index', {stuff: things});
 	});
 }
 

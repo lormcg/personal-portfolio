d18e65e9f6eaf70f41877e646b3c8604726556db
Laura McGinn <mcginn.laura@gmail.com>
2017-07-29 11:42:59 -0500

    disappearing navbar functionality added

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,21 +1,39 @@
 $(document).ready(function() {
 
-	var gradientColors = ['khaki', 'lightsalmon', 'hotpink', 'tomato', 'orchid', 'palegreen', 'turquoise', 'mistyrose', 'lightslategray'];
+//make the navbar disappaear upon scroll down, and re-appear again on scroll up.
+	var lastScrollTop = 0;
+	
+	$(window).scroll(function() {
+		var currentScrollTop = $(this).scrollTop();
 
+		if (currentScrollTop <= lastScrollTop) {
+			$('.navbar').fadeIn();
+			// $('.navbar').addClass('hidden');
+			// $('.navbar').slideDown(400);
+
+		} else {
+			$('.navbar').fadeOut();
+			// $('.navbar').removeClass('hidden');
+			// $('.navbar').slideUp(400);
+			lastScrollTop = currentScrollTop;
+			
+		}
+	});
+	
+//changeColor selects from a set of pre-seleced colors at random upon page load.
 	function changeColor() {
-		var newColor = gradientColors[Math.floor(Math.random() * gradientColors.length)];
+		var gradientColors = ['khaki', 'lightsalmon', 'hotpink', 'tomato', 'orchid', 'palegreen', 'turquoise', 'mistyrose', 'lightslategray'];
 
+		var newColor = gradientColors[Math.floor(Math.random() * gradientColors.length)];
+		//select the end color of the drip gradient on the index.
 		$('#bottom-color').attr('stop-color', newColor);
 	};
 
+	changeColor();
 
+	$('.drips').on('click', changeColor());
 
-	$(document).on('load', changeColor());
-
-	// $('.drips').on('click', changeColor());
-
-
-
+//jQ for paint by numbers
 
 	// $('.palette').on ('click', function() {
 	// 	currentColor = $(this).css('background-color');
@@ -27,4 +45,7 @@ $(document).ready(function() {
 	// $('.canvas').on('click', function() {
 	// 	$(this).css('background-color', currentColor);
 	// });
+
+
+
 });
